80c0b3ef9ab5717fa40ea496bae5b75b562586bb
docfeng <38637196+docfeng@users.noreply.github.com>
2018-12-01 20:21:26 +0800

    update

--- script/app.js
+++ script/app.js
@@ -9,7 +9,7 @@ list.inilist=function(html,fun){
      for(var i=0;i<url_arr.length;i++){
           url_arr[i][0]=url_arr[i][0].getFullUrl(win.url);
      }
-     //alert(url_arr+"\n"+html)
+     alert(url_arr)
      if(!url_arr)return 0;
      //获取小说名称
      var reg_name=new RegExp("<title>(第[^<0-9]*?)<","g");
@@ -26,33 +26,6 @@ list.inilist=function(html,fun){
      var nexturl=html.match(reg);
      var reg2=/<a[^>]*?href="([^"]*?)"[^>]*?>查看完整目录[^<]*?</;
      var nexturl2=html.match(reg2);
-     
-     if(nexturl){
-       nexturl=nexturl[1].getFullUrl(win.url);
-       alert(nexturl);
-       /*getHTML(nexturl,function(html){
-           t.inilist(html,function(arr){
-               var arr=url_arr.concat(arr)
-               //alert(arr)
-               arr=iniarr(arr);
-               fun(arr);
-           })
-        });*/
-     }else{
-        if(nexturl2){
-          nexturl2=nexturl2[1].getFullUrl(this.url);
-          /*getHTML(nexturl2,function(html){
-alert(url_arr)
-            t.inilist(html,function(arr){
-               //alert(arr)
-               var arr=iniarr(arr);
-               fun(arr);
-            })
-          });*/
-        }else{
-          //alert(url_arr)
-          fun(url_arr);
-        }
-     }
+     fun(url_arr);
   }
 list.inilist(html,function(a){alert(a)})
