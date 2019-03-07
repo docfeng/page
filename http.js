5f8ea61f90e393746ac67b58acd55421e3d8a06e
docfeng <38637196+docfeng@users.noreply.github.com>
2019-03-07 11:23:39 +0800

    add

---
+++ http.js
@@ -0,0 +1,21 @@
+//chrome.openFile("../git/noveldownload/noveldownload.html");
+http={
+    xmlhttp:function(){
+        var xmlHttp=null; 
+        try { // Firefox, Opera 8.0+, Safari 
+            xmlHttp=new XMLHttpRequest();
+        }catch (e) { // Internet Explorer 
+            try { 
+                xmlHttp=new ActiveXObject("Msxml2.XMLHTTP"); 
+            }catch (e) { 
+                xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+           } 
+        } 
+        if (xmlHttp==null) { 
+            alert ("您的浏览器不支持AJAX！"); 
+            return; 
+        }
+        return xmlHttp;
+    }
+
+}
