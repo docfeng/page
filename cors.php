673893f896ce0175aa8c91e3a81d883c43767834
docfeng <38637196+docfeng@users.noreply.github.com>
2019-03-09 08:47:11 +0800

    add

---
+++ cors.php
@@ -0,0 +1,23 @@
+<?php
+$ret = array(
+    'name' => isset($_POST['name'])? $_POST['name'] : '',
+    'gender' => isset($_POST['gender'])? $_POST['gender'] : ''
+);
+ 
+header('content-type:application:json;charset=utf8');
+ 
+$origin = isset($_SERVER['HTTP_ORIGIN'])? $_SERVER['HTTP_ORIGIN'] : '';
+ 
+$allow_origin = array(
+    'http://www.client.com',
+    'http://www.client2.com'
+);
+ 
+if(in_array($origin, $allow_origin)){
+    header('Access-Control-Allow-Origin:'.$origin);
+    header('Access-Control-Allow-Methods:POST');
+    header('Access-Control-Allow-Headers:x-requested-with,content-type');
+}
+ 
+echo json_encode($ret);
+?>
