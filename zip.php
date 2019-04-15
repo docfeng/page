e17f34186cf50c2de743bf712194703c510bb7cf
docfeng <38637196+docfeng@users.noreply.github.com>
2019-04-14 22:50:15 -0500

    add

---
+++ zip.php
@@ -0,0 +1,15 @@
+<?php
+$files = ["zip.php","main.php"];
+$filename = "test.zip";
+$zip = new ZipArchive();
+if ($zip->open($filename, ZIPARCHIVE::CREATE) !== TRUE) {
+     exit ('无法打开文件，或者文件创建失败');
+}
+$zip->open($filename,ZipArchive::CREATE);   //打开压缩包
+foreach ($files as $file) {
+  $zip->addFile($file,basename($path));
+  //向压缩包中添加文件
+}
+$zip->close();  //关闭压缩包
+
+?>
