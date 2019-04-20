7b3bd4b13f992a3c1395f4b48b89e1ce8cdc298c
docfeng <38637196+docfeng@users.noreply.github.com>
2019-04-20 02:44:42 -0500

    update

--- script/store.js
+++ script/store.js
@@ -1,61 +1,34 @@
-myStorage=function(f){
-  this.fun();
-  this.init(f);
+store={
+    async init() {
+        var db=new DB();
+        await db.open_db("store");
+        //await db.create_store("store",{key:"name",index:{name:true,val:false}});
+        //db.select_store("store");
+        if(!db.select_store("store")){
+            await db.create_store("store",{key:"name",index:{name:true,val:false}});
+        }
+       return db;
+    },
+    async setItem(name,value){
+      var db=await this.init();
+      var json={"name": name, "val":value };
+      var re=await db.put(json);
+      await db.close_db();
+      return re;
+    },
+    async getItem(name){
+        var db=await this.init();
+        var json={"name": name};
+        var re=await db.getindex(json);
+        try{ 
+          re=re&&re[0]&&re[0].val;
+        }catch(e){alert(false)} 
+        await db.close_db();
+        return re;
+    },
+    async clear(){
+        var db=await this.init();
+        await db.clear_store()
+        await db.close_db();
+    }
 }
-
-myStorage.prototype.fun=function(){
-  var t=this;
-  t.init=function(fun) {
-      var dbParams = new Object();
-      dbParams.db_name = "store1";
-      dbParams.db_version = "1";
-      dbParams.db_store_name = "store";
-      dbObject.init(dbParams,fun);
-  }
-  t.setItem=function(name,value,fun){
-      var json={data:{"name": name, "val":value }, 
-                       success:fun
-                };
-      dbObject.put(json);
-  }
-  t.getItem=function(name,fun){
-      var json={data:{"name": "name", "value":name }, 
-                       success:function(a){fun(a.val)},
-                       error:fun
-                };
-      dbObject.getindex(json);
-  }
-  
-  t.put=function(){
-      // 填入初始值
-      var json={data:{name: "test", val: "test" }, 
-                       key:1,
-                       success:function(a){alert(a)}
-                       };
-      dbObject.put(json);
-  }
- 
-  t.getkey=function(){
-      var json={key:1,success:function(a){alert(JSON.stringify(a))}};
-      dbObject.getkey(json);
-  }
- 
-  t.update=function(){
-      var json={data:{name: "Quarry", val: "ghkkd", isbn: 12566666 }, 
-                           key:1,
-                           success:function(a){alert(a)},
-                           error:function(a){alert(a)}
-                           };
-      dbObject.put(json);
-  }
- 
-  t.delete=function(){
-      dbObject.delete(3);
-  }
- 
-  t.clear=function(){
-      dbObject.clear();
-  }
-}
-
-// myStorage=new myStorage();
