e50f469462d264d7559eb5f2bf86ab3735aff1e3
docfeng <38637196+docfeng@users.noreply.github.com>
2019-04-30 10:49:16 +0800

    add

--- script/base.js
+++ script/base.js
@@ -348,3 +348,22 @@ showEval=function(a){
         ifr1.style.display="block"
     }
 }
+
+
+var browser = (function () {
+    var u = navigator.userAgent;
+    return {//移动终端浏览器版本信息
+        trident: u.indexOf('Trident') > -1, //IE内核
+        presto: u.indexOf('Presto') > -1, //opera内核
+        webKit: u.indexOf('AppleWebKit') > -1, //苹果、谷歌内核
+        gecko: u.indexOf('Gecko') > -1 && u.indexOf('KHTML') == -1, //火狐内核
+        mobile: !!u.match(/AppleWebKit.*Mobile.*/), //是否为移动终端
+        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), //ios终端
+        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, //android终端或uc浏览器
+        iPhone: u.indexOf('iPhone') > -1, //是否为iPhone或者QQHD浏览器
+        iPad: u.indexOf('iPad') > -1, //是否iPad
+        UCBrowser: u.indexOf('UCBrowser') > -1, //UC浏览器
+        MyApp: u.indexOf('MyApp001') > -1, //我的程序
+        webApp: u.indexOf('Safari') == -1 //是否web应该程序，没有头部与底部
+    };
+})();
