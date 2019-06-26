4e7ae3f61666d0aa3e48e0f84822f373538dc228
docfeng <38637196+docfeng@users.noreply.github.com>
2019-06-26 23:24:05 +0800

    add

--- download.js
+++ download.js
@@ -23,8 +23,38 @@ downloadRepos=function(name,user,repos){
     //downloadFile(name,url)
     window.open(url)
 }
+downloadBlob(value, type, name) {
+    var blob;
+    if(typeof window.Blob == "function") {
+        blob = new Blob([value], {type: type});
+    } else {
+        var BlobBuilder = window.BlobBuilder || window.MozBlobBuilder || window.WebKitBlobBuilder || window.MSBlobBuilder;
+        var bb = new BlobBuilder();
+        bb.append(value);
+        blob = bb.getBlob(type);
+    }
+    var URL = window.URL || window.webkitURL;
+    var bloburl = URL.createObjectURL(blob);
+    
+    var anchor = document.createElement("a");
+    if('download' in anchor) {
+        anchor.style.visibility = "hidden";
+        anchor.href = bloburl;
+        anchor.download = name;
+        document.body.appendChild(anchor);
+        var evt = document.createEvent("MouseEvents");
+        evt.initEvent("click", true, true);
+        anchor.dispatchEvent(evt);
+        document.body.removeChild(anchor);
+    } else if(navigator.msSaveBlob) {
+        navigator.msSaveBlob(blob, name);
+    } else {
+        location.href = bloburl;
+    }
+}
 function Save() {
-    //downloadGithubFile("hello.txt","docfeng","page","git.txt");
-    //downloadRepos("h.zip","docfeng","page")
+    downloadGithubFile("hello.txt","docfeng","page","git.txt");
+    //downloadRepos("h.zip","docfeng","page");
+    //downloadBlob("fgggh", "text/csv,charset=UTF-8", "hello.txt");
 }
 Save()
