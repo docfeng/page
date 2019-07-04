6fb4f5f23064fda60a6e0a6f6bc8020013b5c601
docfeng <38637196+docfeng@users.noreply.github.com>
2019-07-04 13:08:31 +0800

    add

--- zip.php
+++ zip.php
@@ -1,15 +1,17 @@
 <?php
 $files = ["zip.php","main.php"];
 $filename = "test.zip";
-$zip = new ZipArchive();
-if ($zip->open($filename, ZIPARCHIVE::CREATE) !== TRUE) {
-     exit ('无法打开文件，或者文件创建失败');
+function createZip($files,$filename){
+    $zip = new ZipArchive();
+    if ($zip->open($filename, ZIPARCHIVE::CREATE) !== TRUE) {
+        exit ('无法打开文件，或者文件创建失败');
+    }
+    $zip->open($filename,ZipArchive::CREATE);   //打开压缩包
+    foreach ($files as $file) {
+        $zip->addFile($file,basename($path));
+        //向压缩包中添加文件
+    }
+    $zip->close();  //关闭压缩包
 }
-$zip->open($filename,ZipArchive::CREATE);   //打开压缩包
-foreach ($files as $file) {
-  $zip->addFile($file,basename($path));
-  //向压缩包中添加文件
-}
-$zip->close();  //关闭压缩包
-
+createZip($files,$filename);
 ?>
