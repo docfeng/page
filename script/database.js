40df1dc93031a74589c49f5dc114acf01d925d9c
docfeng <38637196+docfeng@users.noreply.github.com>
2019-07-19 22:18:41 +0800

    add

--- script/database.js
+++ script/database.js
@@ -9,6 +9,22 @@ var database={
           });
       });
   },
+  tableAdd:function(name,str){
+      var t=this;
+      return new Promise(function(resolve,reject){
+          t.db.transaction(function (tx) {
+              alert(name)
+              var sql="ALTER TABLE ? ADD ?".fill([name,str]);
+              alert(sql)
+              tx.executeSql(sql,[],function(a){
+                  resolve(true)
+              },function(a,e){
+                  alert(e.message)
+                  reject("error: tableAdd:\n"+e.message)
+              });
+          });
+      });
+  },
   insert:function(name,json){
       var t=this;
       var keys=[];
@@ -17,12 +33,20 @@ var database={
          keys.push(key);
          values.push(json[key]);
       }
+      var json=[json];
       //alert(keys)
       //alert(values)
       return new Promise(function(resolve,reject){
           t.db.transaction(function (tx) {  
               //tx.executeSql("CREATE TABLE IF NOT EXISTS "+name+" ("+arr.join(",")+")");
-              tx.executeSql('INSERT INTO '+name+' ('+keys.join(",")+') VALUES (?, ?)', values);
+              //tx.executeSql('INSERT INTO '+name+' ('+keys.join(",")+') VALUES (?,?)',values);
+              var sql="INSERT INTO ? (?) VALUES (?)".fill([name,keys.join(","),values.join(",")]);
+              alert(sql)
+              tx.executeSql(sql,[],function(a){
+                alert(true)
+              },function(a,b){
+                alert(b.message)
+              });
               resolve(true);
           });
       });
@@ -70,11 +94,12 @@ var database={
 
 database.createTable("store",["id unique","log"])
 .then(function(a){
+    database.tableAdd("store","test1")
     return database.select("store")
 })
 .then(function(a){
     alert(JSON.stringify(a))
-    return database.insert("store",{"id":0,"log":"test"});
+    return database.insert("store",{"id":0,"log":"\"test\""});
 }).then(function(a){
     return database.select("store")
 }).then(function(a){
@@ -85,12 +110,15 @@ database.createTable("store",["id unique","log"])
 }).then(function(a){
     alert(JSON.stringify(a))
     return database.delete("store","id=0")
-}).then(function(a){
-    return database.select("store")
-}).then(function(a){
-    alert(JSON.stringify(a))
-    return true
 })
 .catch(function(a){
     alert("err:\n"+a)
 })
+
+String.prototype.fill=function(arr){
+    var str=this
+    for(var i=0;i<arr.length;i++){
+        str=str.replace("?",arr[i]);
+    }
+    return str;
+}
