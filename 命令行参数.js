973ad9f697e820f91f6a33f6e2ee747ab2a01cbd
docfeng <38637196+docfeng@users.noreply.github.com>
2019-11-09 08:11:41 +0800

    add

--- 命令行参数.js
+++ 命令行参数.js
@@ -5,3 +5,7 @@ vbscript:
 hta:
     var str = hta_id.commandLine;
     <hta:application id="testhta">
+    
+    
+    
+    test = createobject("Scripting.FileSystemObject").GetFile(Wscript.ScriptFullName).ParentFolder.Path
