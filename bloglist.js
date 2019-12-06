ef5f12ec7c1bb91cce6aa202462f873bf780ca6a
docfeng <38637196+docfeng@users.noreply.github.com>
2019-12-06 08:07:10 -0600

    add

--- bloglist.js
+++ bloglist.js
@@ -1,15 +1,27 @@
 var user="docfeng"
 var repos_name="blog4";
-var blogListURL = 'https://api.github.com/repos/' + user + '/' + repos_name + '/contents/blog';
-http.get(blogListURL).then(function(json){
-        var json =JSON.parse(json);
-        var re=[];
+
+var getlist=async function(path){
+    var path=path||"";
+    var blogListURL = 'https://api.github.com/repos/' + user + '/' + repos_name + '/contents/blog';
+    var json=await http.get(blogListURL+path);
+    json =JSON.parse(json);
+        var file=[];
+        var dir=[];
         for (var i = 0; i < json.length; i++) {
             var name = json[i].name; // Blog title
             var url= json[i].download_url;
             var type=json[i].type;
-            re.push({name,url,type});
+            if(type=="dir"){
+                var r=await getlist(path+"/"+name);
+                dir.push([name,r]);
+            }else{
+                file.push({name,url,type});
+            }
         }
+        return [dir,file]
+}
+getlist().then(function(re){
         alert(JSON.stringify(re,null,4))
         //http.get(re[0].url).then(function(a){alert(a)})
-})
+});
