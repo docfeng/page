2c70a2fa71631cb397997733a1bedee5b52c97a6
docfeng <38637196+docfeng@users.noreply.github.com>
2020-01-31 08:22:19 +0800

    add

---
+++ pwa/sw.js
@@ -0,0 +1,16 @@
+//importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-1'
+var cacheList=[
+  '/',
+  'index.html',
+  'main.css',
+  'youhun.jpg'
+]
+alert()
+self.addEventListener('install',e =>{
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
