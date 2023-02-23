3ae4eb0f4406faecf3ca2c6c90537b9f37942718
docfeng <38637196+docfeng@users.noreply.github.com>
2023-02-23 23:36:36 +0800

    yt2.0

---
+++ yt/script/ui.js
@@ -0,0 +1,9 @@
+"use strict";
+var UI = {
+    showDialog: function (id) {
+        document.getElementById(id).style.display = "block";
+        //window.parentNode.style.display = "block";
+        //contextmenu.style.top = window.screen.availHeight/2 + "px";
+        //contextmenu.style.left =window.screen.availWidth/2 + "px";
+    }
+}
